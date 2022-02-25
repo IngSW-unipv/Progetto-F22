bcae04b1708dbd69e3d6f745986d93ad59308546
tommasomasaracchio <97732941+tommasomasaracchio@users.noreply.github.com>
2022-02-25 11:33:48 +0100

    Add files via upload

---
+++ Codice/View/gui/src/panelspackage/panels/Impostazioni.java
@@ -0,0 +1,86 @@
+package panelspackage.panels;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import panelspackage.panels.elements.AddPostButton;
+import panelspackage.panels.elements.AreaDiTesto;
+import panelspackage.panels.elements.Box;
+import panelspackage.panels.elements.Etichette;
+import panelspackage.panels.elements.SpecificContainer;
+
+public class Impostazioni extends JPanel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	Color ARANCIONE = new Color(255, 125, 0);
+	Color NERO = new Color(0, 0, 0);
+	static String visibilita[]  = {"Pubblico", "Privato"};
+	static String colori[] = {"arancione", "verde"};
+	static String sesso[] = {"maschio", "femmina","altro"};
+	public Impostazioni(String profilo) {
+	
+		this.setOpaque(true);
+		this.setVisible(true);
+		this.setLayout(new BorderLayout());
+		this.setBackground(NERO);
+		
+		SpecificContainer containerNorth = new SpecificContainer();
+		this.add(containerNorth, BorderLayout.NORTH);
+		
+		JLabel nomeProfilo = new JLabel(profilo);
+		nomeProfilo.setVisible(true);
+		
+		nomeProfilo.setForeground(ARANCIONE);
+		containerNorth.add(nomeProfilo, BorderLayout.CENTER);
+		
+		SpecificContainer containerWest = new SpecificContainer(ARANCIONE);
+		this.add(containerWest, BorderLayout.WEST);
+		
+
+		containerWest.setLayout(new GridLayout(8,1));
+		
+		containerWest.add(new AddPostButton("Modifica profilo", NERO));
+		containerWest.add(new AddPostButton("Cambia profilo", ARANCIONE));
+		containerWest.add(new Box(getVisibilita(), ARANCIONE));
+		containerWest.add(new AddPostButton("Visibilità post", ARANCIONE));
+		containerWest.add(new Box(getColori(), ARANCIONE));
+		containerWest.add(new AddPostButton("Logout", ARANCIONE));
+		containerWest.add(new AddPostButton("Elimina Account", ARANCIONE));
+
+		
+		SpecificContainer containerCenter = new SpecificContainer(ARANCIONE);
+		this.add(containerCenter, BorderLayout.CENTER);
+		
+
+		containerCenter.setLayout(new GridLayout(4,1));
+		
+		containerCenter.add(new AreaDiTesto(ARANCIONE, "inserisci la tua nuova descrizione profilo"));
+		containerCenter.add(new Box(getSesso(), ARANCIONE));
+		containerCenter.setVisible(false);
+
+		SpecificContainer containerSouth = new SpecificContainer();
+		this.add(containerSouth, BorderLayout.SOUTH);
+		
+		
+		Etichette etichettaSud = new Etichette("Sezione Inferiore", ARANCIONE);
+		containerSouth.add(etichettaSud, BorderLayout.CENTER);
+	}
+	public String[] getVisibilita() {
+		return visibilita;
+	}
+
+	public String[] getColori() {
+		return colori;
+	}
+
+	public String[] getSesso() {
+		return sesso;
+	}
+
+}
+	
---
+++ Codice/View/gui/src/panelspackage/panels/Profilo.java
@@ -0,0 +1,81 @@
+
+package panelspackage.panels;
+import javax.swing.*;
+
+
+import java.awt.*;
+import java.util.ArrayList;
+
+import panelspackage.panels.elements.GrigliaDiElementi;
+import panelspackage.panels.elements.LabeledIcon;
+import panelspackage.panels.elements.AddPostButton;
+import panelspackage.panels.elements.AreaDiTesto;
+import panelspackage.panels.elements.Etichette;
+import panelspackage.panels.elements.SpecificContainer;
+public class  Profilo extends JPanel {
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	ArrayList<JComponent> ListaEtichette = new ArrayList<JComponent>();
+	ArrayList<String> dati = new ArrayList<String>();
+	Color ARANCIONE = new Color(255, 125, 0);
+	Color NERO = new Color(0, 0, 0);
+	int i;
+	
+	public Profilo(String NickName, int numeroFollowers, int numeroSeguiti, int numeroPost, String immagineProfilo, String[] immaginiPost) {
+		
+		this.setOpaque(true);
+		this.setVisible(true);
+		this.setLayout(new BorderLayout());
+		this.setBackground(NERO);	
+		
+		SpecificContainer containerNorth = new SpecificContainer();
+		this.add(containerNorth, BorderLayout.NORTH);
+		LabeledIcon immagine = new LabeledIcon(immagineProfilo);
+		containerNorth.add(immagine, BorderLayout.WEST);
+		
+		Etichette etichetta = new Etichette(NickName, ARANCIONE);
+		containerNorth.add(etichetta, BorderLayout.NORTH);
+		
+		
+		this.dati.add("N.Post");
+		this.dati.add(Integer.toString(numeroPost));
+		this.dati.add("N.Follower");
+		this.dati.add(Integer.toString(numeroFollowers));
+		this.dati.add("N.Seguiti");
+		this.dati.add(Integer.toString(numeroSeguiti));
+		for( i = 0; i < this.dati.size(); i++) {
+			Etichette area = new Etichette(this.dati.get(i), ARANCIONE);
+			ListaEtichette.add(area);
+		}
+		
+		GrigliaDiElementi Dati =  new GrigliaDiElementi(ListaEtichette,3,2, ListaEtichette.size());
+		containerNorth.add(Dati, BorderLayout.SOUTH);
+		
+		SpecificContainer containerCenter = new SpecificContainer(NERO, ARANCIONE);
+		this.add(containerCenter, BorderLayout.CENTER);
+		containerCenter.setLayout(new GridLayout(3,3));
+
+		for( i = 0; i < immaginiPost.length || i > 9 ; i++) {
+			containerCenter.add(new LabeledIcon(immaginiPost[i]));
+		}
+		
+		SpecificContainer containerSouth = new SpecificContainer(NERO);
+		this.add(containerSouth, BorderLayout.SOUTH);
+		
+		Etichette etichettaSud = new Etichette("Sezione Inferiore", ARANCIONE);
+		containerSouth.add(etichettaSud, BorderLayout.CENTER);
+	}
+
+	public ArrayList<JComponent> getListaAreaTesto() {
+		return ListaEtichette;
+	}
+
+	public void setListaAreaTesto(ArrayList<JComponent> listaAreaTesto) {
+		ListaEtichette = listaAreaTesto;
+	}
+}
