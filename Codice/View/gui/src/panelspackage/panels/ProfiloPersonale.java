6a4d0b1d5719685ee7849d57aa5c0143121c488b
tommasomasaracchio <tommasomasaracchio@169.254.36.201>
2022-02-25 20:46:59 +0100

    aggiornamento

--- Codice/View/gui/src/panelspackage/panels/ProfiloPersonale.java
+++ Codice/View/gui/src/panelspackage/panels/ProfiloPersonale.java
@@ -2,12 +2,16 @@ package panelspackage.panels;
 
 public class ProfiloPersonale  extends Profilo {
 	
+	public ProfiloPersonale(String NickName, int numeroFollowers, int numeroSeguiti, int numeroPost,
+			String immagineProfilo, String[] immaginiPost) {
+		super(NickName, numeroFollowers, numeroSeguiti, numeroPost, immagineProfilo, immaginiPost);
+		// TODO Auto-generated constructor stub
+	}
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	ProfiloPersonale(String NickName) {
-		super(NickName);
-	}
+	
 }
