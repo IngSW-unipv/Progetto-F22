b9c2238277d44c81e827b09d4ea64f0f28b93726
99mami <mamikane99@gmail.com>
2022-03-03 13:37:03 +0100

    Piccoli cambiamenti sul profilo

--- Codice/View/gui/src/profilo/IProfilo.java
+++ Codice/View/gui/src/profilo/IProfilo.java
@@ -10,13 +10,12 @@ public interface IProfilo {
 
 	public HashMap<String, String> modificaFollow(Profilo p);
 	public boolean modificaLike(Profilo p);
-	public Profilo cercaProfilo(Profilo p);
+	public Profilo cercaProfilo(Profilo p) throws Exception;
 	public boolean modificaDislike(Profilo profilo);
 	public void vediStory();  // con tipo Story(da modificare)
-	public void commentare(String testo);
+	public String commentare(String testo);
 	public String bloccaUtente(Utente u); // O profilo.
-	public void mostraInformazioniProfiloPubblico(Profilo p);
-	public void mostraInformazioniProfiloPrivato(Profilo p);
+	public void mostraInformazioniProfilo(Profilo p);
 	public void mostraInformazioniPost(Post p);
 	
 	}
--- Codice/View/gui/src/profilo/ProfiloTester.java
+++ Codice/View/gui/src/profilo/ProfiloTester.java
@@ -4,10 +4,10 @@ import java.util.HashMap;
 
 public class ProfiloTester {
 	
-    public static void main(String [] args) {
+    public static void main(String [] args) throws Exception {
 	
-	Profilo p1 = new Profilo("P00", null, null, 0, 0, 0, null, null, null, null, null);
-	Profilo p2 = new Profilo("P01", null, null, 0, 0, 0, null, null, null, null, "myPost");
+	Profilo p1 = new Profilo("P00", "MyAccount", null, 0, 0, 0, EnumProfilo.PRIVATO, null, null, null, null);
+	Profilo p2 = new Profilo("P01", null, null, 0, 0, 0, EnumProfilo.PUBBLICO, null, null, null, "myPost");
 	Profilo p3 = new Profilo("P02", "nocera", null, 0, 0, 0, null, null, null, null, null);
 	HashMap <String, String> res = p1.modificaFollow(p2);
 	//HashMap <String, String> res = p1.modificaFollow(p2);
@@ -18,15 +18,16 @@ public class ProfiloTester {
 	    System.out.println(key + " " + value);
 	}
 
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		//System.out.println(p1);
+		//System.out.println(p2);
+		//System.out.println(p3);
 		System.out.println(p1.cercaProfilo(p3));
 		
-		p1.mostraInformazioniProfiloPubblico(p2);
 		
-
-
+		
+		//p2.mostraInformazioniProfilo(p1);
+		//p3.mostraInformazioniProfilo(p2);
+		
  }
     
 }
