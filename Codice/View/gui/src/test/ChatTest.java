8efb98587c710951745f5299a97d3ca8b6575e63
FrancescoArdizzoni <97731827+Ardizzoni99@users.noreply.github.com>
2022-03-04 16:43:26 +0100

    Update ChatTest.java

--- Codice/View/gui/src/test/ChatTest.java
+++ Codice/View/gui/src/test/ChatTest.java
@@ -4,16 +4,20 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 
+import chat.Chat;
+import chat.chatDiGruppo.ChatDiGruppo;
+import chat.chatPrivata.ChatPrivata;
+
 class ChatTest {
 
 	@Before
-	public void initTest() {
+	public void initTestChat() {
 		
 	}
 	
 	
 	@Test
-	public void test() {
+	public void testChat() {
 		
 	}
 }
