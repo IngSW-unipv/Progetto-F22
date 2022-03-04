613e0d7a7223492525438f6e46ccdef67e4cc769
FrancescoArdizzoni <97731827+Ardizzoni99@users.noreply.github.com>
2022-03-04 17:02:47 +0100

    Update ChatTest.java

--- Codice/View/gui/src/test/ChatTest.java
+++ Codice/View/gui/src/test/ChatTest.java
@@ -7,14 +7,22 @@ import org.junit.Test;
 import chat.Chat;
 import chat.chatDiGruppo.ChatDiGruppo;
 import chat.chatPrivata.ChatPrivata;
+import profilo.Profilo;
 
 class ChatTest {
-
+	
+	private Chat c1, c2;
+	private Profilo p;
+	
+	/**
+	 * Crea due chat che poi comparer.
+	 * Crea inoltre due liste di ruoli per istanziare correttamente i due compiti.
+	 * */
 	@Before
 	public void initTestChat() {
-		
+		c1 = new ChatDiGruppo(p);
+		c2 = new ChatPrivata(p);
 	}
-	
 	
 	@Test
 	public void testChat() {
