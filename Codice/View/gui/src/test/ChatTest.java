3ce7a692f2471bc9033e9543bd61a7db1a8b3fac
franc <franc@LAPTOP-7HTBFU8R>
2022-03-04 16:39:08 +0100

    inizio a mettere giu il test della chat

---
+++ Codice/View/gui/src/test/ChatTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+
+class ChatTest {
+
+	@Before
+	public void initTest() {
+		
+	}
+	
+	
+	@Test
+	public void test() {
+		
+	}
+}
