59e9350553db2d961f4122d4b37526f7d16bc0f7
FrancescoArdizzoni <97731827+Ardizzoni99@users.noreply.github.com>
2022-03-14 17:17:14 +0100

    Update ChatTest.java

--- Codice/View/gui/src/test/ChatTest.java
+++ Codice/View/gui/src/test/ChatTest.java
@@ -43,38 +43,22 @@ import Messaggio.MessaggioPrivato;
 class ChatTest {
 	
 	private Chat c1, c2;
-<<<<<<< HEAD
+
 	private Profilo p;
 	private ArrayList<Messaggio> listaMessaggi = new ArrayList<Messaggio>();
-=======
+
 	private ArrayList<Messaggio> listaMessaggi;
 	private Messaggio m1, m2;
->>>>>>> branch 'main' of https://github.com/IngSW-unipv/Progetto-F22.git
-	
-<<<<<<< HEAD
+
 	/**
 	 * Crea due chat che poi comparerò.
 	 * Crea inoltre due liste di ruoli per istanziare correttamente i due compiti.
 	 * */
-=======
+
 	//verifico che posso istanziare correttamente le chat
->>>>>>> branch 'main' of https://github.com/IngSW-unipv/Progetto-F22.git
 	@Before
 	public void initTestChat() {
-		
-<<<<<<< HEAD
-		p = new Profilo("P001", "profiloA", "descrizione del profilo", 235, 384, 21, EnumProfilo.PRIVATO, "002", "001", "francesco.ardizzoni@gmail.com", "postX");
-		
-		Gruppo g = new Gruppo("g001", "descrizione del gruppo", "TMF-JAVA", p.getNickname(), "profiloB", "profiloC", "profiloD", "profiloE", "profiloF", "profiloB");
-		Messaggio m2 = new MessaggioDiGruppo(p.getMessaggioDiGruppo(), null, null, "Tutto bene grazie", "G01", g.getIdGruppo());
-		c1 = new ChatDiGruppo(p);
-		
-		Messaggio m1 = new MessaggioPrivato(p.getMessaggioPrivato(), null, null, "Ciao Sara, come va?", "G00", "P001", "P003");
-		c2 = new ChatPrivata(p);
-		
-		listaMessaggi.add(m1);
-		listaMessaggi.add(m2);
-=======
+
 		listaMessaggi = new ArrayList<Messaggio>();
 		
 		Profilo p = new Profilo("P001", "profiloA", "descrizione del profilo", 235, 384, 21, EnumProfilo.PRIVATO, "002", "001", "francesco.ardizzoni@gmail.com", "postX");
@@ -86,8 +70,6 @@ class ChatTest {
 		m1 = new MessaggioPrivato("ciao", null, null, "Ciao Sara, come va?", "G00", "P001", "P003");
 		c1 = new ChatPrivata(p);
 		
-		
-		
 		listaMessaggi.add(m1);
 		listaMessaggi.add(m2);		
 	}
@@ -97,7 +79,6 @@ class ChatTest {
 		
 		assertTrue(c1.caricaMessaggio(listaMessaggi.get(0)));
 		assertTrue(c2.caricaMessaggio(listaMessaggi.get(1)));
->>>>>>> branch 'main' of https://github.com/IngSW-unipv/Progetto-F22.git
 	}
 	
 	@Test
