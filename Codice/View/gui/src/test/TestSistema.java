82a98f2530bc9467e0f3e9f49fd8b94c1e2725d4
masaracchiotommaso <tommaso.masaracchio01@unipv.it>
2022-05-13 12:00:35 +0200

    testing sistema

--- Codice/View/gui/src/test/TestSistema.java
+++ Codice/View/gui/src/test/TestSistema.java
@@ -13,82 +13,93 @@ import profilo.Profilo;
 import profilo.exception.*;
 
 class TestSistema {
-	
-	Sistema sistema; 
-	boolean c;
-	
-	public static void main(String[] args)   {
+
+	static Sistema sistema; 
+	static boolean esitoTestSignIn, esitoRimozione;
+
+	public static void main(String[] args) throws AccountDoesNotExist, AccountGiaEsistente   {
 		Sistema sistema = new Sistema();
-		sistema.rimuoviAccount(new Profilo("ciccioGamer@unipv.it","ciccio"));
+
+		esitoRimozione = sistema.rimuoviAccount(new Profilo("ciccioGamer@unipv.it","ciccio"));
 
 		try {
-			boolean c;
-			c = sistema.signIn("ciccioGamer@unipv.it", "ciccio");
-			System.out.println(c);
+			esitoTestSignIn = sistema.signIn("ciccioGamer@unipv.it", "ciccio");
+			System.out.println(esitoRimozione);
+
 		} catch (AccountGiaEsistente e) {
 			e.printStackTrace();
 		}
-		
-		
+
 		try {
 			sistema.cambiaDefaultPassword("ciccioGamer@unipv.it", "Gelato");
 		} catch (ChangeDefaultPassword e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (AccountDoesNotExist e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+	 catch (AccountDoesNotExist e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+	 }
+
+
+	 try {
+		 try {
+			 sistema.cambiaPassword("ciccioGamer@unipv.it","Mucca", "Lucertola");
+		 } catch (ChangePassword e) {
+			 // TODO Auto-generated catch block
+			 e.printStackTrace();
+		 }
+	 } catch (ChangeDefaultPassword e) {
+		 // TODO Auto-generated catch block
+		 e.printStackTrace();
+	 } catch (AccountDoesNotExist e) {
+		 // TODO Auto-generated catch block
+		 e.printStackTrace();
+	 }
+
+	 try {
+		 sistema.login("ciccioGamer@unipv.it", "Mucca");
+	 } catch (ChangeDefaultPassword e) {
+		 // TODO Auto-generated catch block
+		 e.printStackTrace();
+	 } catch (AccountDoesNotExist e) {
+		 // TODO Auto-generated catch block
+		 e.printStackTrace();
+	 } catch (PswOmailErrati e) {
+		 // TODO Auto-generated catch block
+		 e.printStackTrace();
+	 }
+
+	 testSignIn();
 		}
-		
-		
-		try {
+
+		@BeforeAll
+		static void setUpBeforeClass() { 
+			Sistema sistema = new Sistema();
+			esitoRimozione = sistema.rimuoviAccount(new Profilo("ciccioGamer@unipv.it","ciccio"));
+		}
+
+		@AfterAll
+		static void tearDownAfterClass() throws Exception {
+		}
+
+		@BeforeEach
+		void setUp() throws AccountGiaEsistente {
+
+		}
+
+		@AfterEach
+		void tearDown() throws Exception {
+		}
+
+		@Test
+		static	void testSignIn() throws AccountGiaEsistente {
 			try {
-				sistema.cambiaPassword("ciccioGamer@unipv.it","Mucca", "Lucertola");
-			} catch (ChangePassword e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		} catch (ChangeDefaultPassword e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (AccountDoesNotExist e) {
+				esitoTestSignIn = sistema.signIn("ciccioGamer@unipv.it", "ciccio");
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		try {
-			sistema.login("ciccioGamer@unipv.it", "Mucca");
-		} catch (ChangeDefaultPassword e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (AccountDoesNotExist e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (PswOmailErrati e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		assertTrue(esitoTestSignIn);
 	}
-
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
-
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-
 }
+
