9e5dda76e198a307b3ef74c49865336addd36598
99mami <mamikane99@gmail.com>
2022-06-04 23:16:53 +0200

    Ripristino credenziali della connessione del database

--- Database/DBPersistenza/src/db/connessione/DBConnection.java
+++ Database/DBPersistenza/src/db/connessione/DBConnection.java
@@ -15,8 +15,8 @@ public class DBConnection {
 
 		DbDriver = "com.mysql.cj.jdbc.Driver";
 		DbURL = "jdbc:mysql://127.0.0.1:3306/socialnetwork";
-		username = "aissa";
-		password = "";
+		username = "root";
+		password = "Dadeinter99";
 		if ( isOpen(conn) )
 			closeConnection(conn);
 		try
