03194edcf5765dad86d46c26130b01db72c58646
FrancescoArdizzoni <97731827+Ardizzoni99@users.noreply.github.com>
2022-06-06 11:22:17 +0200

    fix impostazioni

--- Codice/View/gui/src/panelspackage/panels/Impostazioni.java
+++ Codice/View/gui/src/panelspackage/panels/Impostazioni.java
@@ -22,15 +22,11 @@ import panelspackage.panels.elements.SpecificContainer;
 public class Impostazioni extends JPanel {
 	HashMap<String,JComponent> mappaPulsanti = new  HashMap<String,JComponent>();
 	private Pulsanti homeImpostazioni, salvaModifiche;
-	private AreaDiTesto areaDescrizione, areaNuovaMail;
-	private Box areaCambioSesso;
+	private AreaDiTesto areaDescrizione;
 	private SpecificContainer containerCenter;
 	
 	int i;
-	private static String visibilita[]  = {"Pubblico", "Privato"};
 	private static String colori[] = {"arancione", "verde"};
-	private static String sesso[] = {"maschio", "femmina","altro"};
-	private static String profili[] = {"Profilo1", "Profilo2", "Profilo3", "Profilo4"};
 	
 	public Impostazioni(String profilo) {
 		avvio();
@@ -58,9 +54,6 @@ public class Impostazioni extends JPanel {
 		containerWest.setLayout(new GridLayout(8,1));
 		
 		mappaPulsanti.put("modificaprofilo", new Pulsanti("Modifica profilo", Frame.COLOREPRIMARIOTEMATICO));
-		//mappaPulsanti.put("cambiaprofilo",new Pulsanti("Cambia profilo", Frame.COLOREPRIMARIOTEMATICO));
-		mappaPulsanti.put("cambiaProfilo", new Box(getProfili(), Frame.COLOREPRIMARIOTEMATICO));
-		mappaPulsanti.put("visibilita", new Box(getVisibilita(), Frame.COLOREPRIMARIOTEMATICO));
 		mappaPulsanti.put("visibilitapost", new Pulsanti("Visibilita' post", Frame.COLOREPRIMARIOTEMATICO));
 		mappaPulsanti.put("cambiaColori", new Box(getColori(), Frame.COLOREPRIMARIOTEMATICO));
 		mappaPulsanti.put("logout", new  Pulsanti("Logout", Frame.COLOREPRIMARIOTEMATICO));
@@ -74,8 +67,6 @@ public class Impostazioni extends JPanel {
 		containerCenter.setLayout(new GridLayout(4,1));
 
 		containerCenter.add(areaDescrizione = new AreaDiTesto(Frame.COLOREPRIMARIOTEMATICO, "inserisci la tua nuova descrizione profilo"));
-		containerCenter.add(areaCambioSesso = new Box(getSesso(), Frame.COLOREPRIMARIOTEMATICO));
-		containerCenter.add(areaNuovaMail= new AreaDiTesto(Frame.COLOREPRIMARIOTEMATICO, "inserisci una nuova mail"));
 		containerCenter.setVisible(false);
 
 		SpecificContainer containerSouth = new SpecificContainer();
@@ -85,36 +76,15 @@ public class Impostazioni extends JPanel {
 		containerSouth.add(homeImpostazioni = new Pulsanti("Torna alla home", Frame.COLORESECONDARIOTEMATICO),BorderLayout.CENTER);
 	}
 	
-	public String[] getVisibilita() {
-		return visibilita;
-	}
-	
 	public String[] getColori() {
 		return colori;
 	}
-	
-	public String[] getSesso() {
-		return sesso;
-	}
-	
-	public String[] getProfili() {
-		return profili;
-	}
-	
 	
 	public SpecificContainer getContainerCenter() {
 		return this.containerCenter;
 	}
 	public Pulsanti getModificaProfilo() {
 		return (Pulsanti) mappaPulsanti.get("modificaprofilo");
-	}
-	
-	public Box getCambiaProfilo() {
-		return (Box) mappaPulsanti.get("cambiaProfilo");
-	}
-	
-	public Box getVisibilitaProfilo() {
-		return (Box) mappaPulsanti.get("visibilita");
 	}
 	
 	public Box getCambiaColore() {
