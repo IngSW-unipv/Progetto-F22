f65a177416ce5a6a4764ee59c34fd786b710309b
tommasomasaracchio <tommaso.masaracchio01@unipv.it>
2022-06-15 20:40:10 +0200

    bug numero idee maggiore di 3

--- Codice/src/panelspackage/panels/Profilo.java
+++ Codice/src/panelspackage/panels/Profilo.java
@@ -226,7 +226,7 @@ public class  Profilo extends JPanel {
 				}
 			}
 			if(postTestoDelProfilo.size() > 0)	 {
-				for ( i = 0; i < postTestoDelProfilo.size()/2 ; i++) {
+				for ( i = 0; i < postTestoDelProfilo.size()/2 && i < 3  ; i++) {
 					ListaTesti.get(i).setText(postTestoDelProfilo.get(((i*2) + 1) + this.getContatoreTesto()));
 				}
 			}
